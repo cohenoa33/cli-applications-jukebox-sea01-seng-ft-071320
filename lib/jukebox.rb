5ff6d3ab3df9fcc1d727541b0d0edef73d5e9f4a
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-07-12 00:14:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -39,14 +39,14 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  user_response = gets.strip
+  user_response = gets.strip.to_s
 
-    if (1...9).include?(user_response).to_i
+    if (1...9).to_a.include?(user_response.to_i)
      puts "Playing #{song[user_response.to_i -1]}"
-    elsif songs.include?(user_response).to_s
+    elsif songs.include?(user_response)
      puts "Playing #{(user_response)}"
     else
-     puts "Invalid input, please try again"
+      puts "Invalid input, please try again"
    end
 
   
